d33f7cfdfca329bd0ccf3c1fa3f26057e8371614
ElmirKuba <elmir_kuba@vk.com>
2025-07-28 14:10:49 +0500

    [front] Почищен от лишнего, что создает генератор проекта nx

--- nx-monorepo/apps/angular-frontend/src/app/app.spec.ts
+++ nx-monorepo/apps/angular-frontend/src/app/app.spec.ts
@@ -9,12 +9,12 @@ describe('App', () => {
     }).compileComponents();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome angular-frontend'
-    );
-  });
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(App);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('h1')?.textContent).toContain(
+  //     'Welcome angular-frontend'
+  //   );
+  // });
 });
